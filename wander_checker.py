ea0e257c37169251ad99863623bb26112aad8a07
parkspark <rkatk1760@gmail.com>
2024-03-01 04:46:45 +0900

    Code functionalization

--- wander_checker.py
+++ wander_checker.py
@@ -16,25 +16,24 @@ webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
 # 확인할 사이트 URL
 TARGET_URL = "https://kloa.gg/merchant"
 
-driver = webdriver.Chrome() 
-driver.get(TARGET_URL)
-time.sleep(2)
+sub_strs = ["카마인","웨이", "아만 카드", "아제나","바훈투르","바르칸","에버그레이스","모카모카","라하르트","페데리코","다르시","라자람","라카이서스"]
 
-#서버선택
-element = driver.find_element(By.ID, "headlessui-combobox-input-:r2:")
-element.click()
 
-time.sleep(0.5)
-#루페온
-lupeon_element = driver.find_element(By.XPATH, "//span[text()='루페온']")
-lupeon_element.click()
+def server_select(server):
+    time.sleep(0.5)
+    
+    #서버목록 선택
+    element = driver.find_element(By.ID, "headlessui-combobox-input-:r2:")
+    element.click()
 
-time.sleep(0.5)
-div_text = driver.find_element(By.CSS_SELECTOR, 'div.mb-\\[30px\\]').text
+    time.sleep(0.5)
+    # #루페온
+    # lupeon_element = driver.find_element(By.XPATH, "//span[text()='루페온']")
+    select_server = driver.find_element(By.XPATH, f"//span[text()='{server}']")
+    select_server.click()
 
 
 def send_discord_message(webhook_url, message):
-    
     data = {
         "content": message
     }
@@ -43,16 +42,27 @@ def send_discord_message(webhook_url, message):
     
     if response.status_code != 204:
         raise ValueError(f"Request to discord returned an error {response.status_code}, the response is:\n{response.text}")
+    
 
-def check_strings(main_str, sub_strs):
+def check_strings(main_str, sub_strs, server):
     for sub_str in sub_strs:
         if sub_str in main_str:
-            discord_send_message = f"{datetime.now().strftime('%H:%M')} {sub_str} 출현"
+            discord_send_message = f"{server}서버에 {datetime.now().strftime('%H:%M')} {sub_str} 출현"
             send_discord_message(webhook_url, discord_send_message)
         else:
             print(f"'{sub_str}' 없음")
             
-sub_strs = ["웨이", "아만", "아제나","바훈"]
+def card_checker(server):
+    server_select(server)
+    time.sleep(0.5)
+    div_text = driver.find_element(By.CSS_SELECTOR, 'div.mb-\\[30px\\]').text
+    time.sleep(0.5)
+    check_strings(div_text, sub_strs, server)
+    
+    
+driver = webdriver.Chrome() 
+driver.get(TARGET_URL)
+time.sleep(2)
 
-
-check_strings(div_text, sub_strs)
+card_checker("루페온")
+card_checker("아만")
