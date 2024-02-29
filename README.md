69f7cefba51eb607d358839d510fdc4dbf09b534
parkspark <rkatk1760@gmail.com>
2024-02-29 18:46:35 +0900

    up

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Loa_Wandering_merchant
-로아 떠돌이 상인 물품 알림
+
+![로아 떠상 알림봇](/image/wei.png)
+
+로아 떠돌이 상인 물품 알림봇
+
+
---
+++ wander_checker.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import requests
+import json
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+
+# 디스코드 웹훅 URL
+load_dotenv()
+webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
+
+# 확인할 사이트 URL
+TARGET_URL = "https://kloa.gg/merchant"
+
+driver = webdriver.Chrome() 
+driver.get(TARGET_URL)
+time.sleep(2)
+
+#서버선택
+element = driver.find_element(By.ID, "headlessui-combobox-input-:r2:")
+element.click()
+
+time.sleep(0.5)
+#루페온
+lupeon_element = driver.find_element(By.XPATH, "//span[text()='루페온']")
+lupeon_element.click()
+
+time.sleep(0.5)
+div_text = driver.find_element(By.CSS_SELECTOR, 'div.mb-\\[30px\\]').text
+
+
+def send_discord_message(webhook_url, message):
+    
+    data = {
+        "content": message
+    }
+    response = requests.post(webhook_url, data=json.dumps(data),
+                             headers={"Content-Type": "application/json"})
+    
+    if response.status_code != 204:
+        raise ValueError(f"Request to discord returned an error {response.status_code}, the response is:\n{response.text}")
+
+def check_strings(main_str, sub_strs):
+    for sub_str in sub_strs:
+        if sub_str in main_str:
+            discord_send_message = f"{datetime.now().strftime('%H:%M')} {sub_str} 출현"
+            send_discord_message(webhook_url, discord_send_message)
+        else:
+            print(f"'{sub_str}' 없음")
+            
+sub_strs = ["웨이", "아만", "아제나","바훈"]
+
+
+check_strings(div_text, sub_strs)
